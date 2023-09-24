f12756f7f2fb4bef8271a51bc8e11f950f9b62d7
vavaste <stefano.vavassori@codestorm.it>
2023-09-24 19:08:35 +0200

    add jwtsecret and jwtexpiration

---
+++ counter/src/main/java/progetto/react/com/counter/payload/request/AddTodoRequest.java
@@ -0,0 +1,14 @@
+package progetto.react.com.counter.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddTodoRequest {
+    String todo;
+}
